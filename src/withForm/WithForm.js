1a15a98b07f31babf88f3e3894c0952292375401
Napat Maipaiboon <napat@DHA-iMac-8.local>
2017-10-24 14:59:26 +0700

    call function to bind control to rule at initial and update value

--- src/withForm/WithForm.js
+++ src/withForm/WithForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { validateControl,isObject } from './../utils/utils';
+import { validateControl,isObject,bindControlToRule } from './../utils/utils';
 const withForm = function(Wrappedcomp,controls, formpropname="form"){
     if(!isObject(controls) || Object.keys(controls).length <= 0){
         throw new Error('you need to provide controls in secocde parameter');
@@ -21,30 +21,32 @@ const withForm = function(Wrappedcomp,controls, formpropname="form"){
                     this.state.values[name] = controls[name][0];
                 }
                 
+                const bindrules = bindControlToRule(this.state.rules[name],this.state.values);
 
-                this.state.errors[name] = validateControl(this.state.values[name],this.state.rules[name],name);
+                this.state.errors[name] = validateControl(this.state.values[name],bindrules,name);
             });
         }
 
         updateValue(name,value){
             const newValues = {
+                ...this.state.values,
                 [name]:value
             };
-            const newErrors = {
-                [name]:validateControl(value,this.state.rules[name],name)
-            };
-            this.setState(({errors={},values={}})=>{
+            console.log(newValues,'newValues');
+
+            let newErrors = {};
+            Object.keys(controls).map((name) => {
+                const bindrules = bindControlToRule(this.state.rules[name],newValues);
+                newErrors[name] = validateControl(newValues[name],bindrules,name);
+            });
+        
+            this.setState(({errors={}})=>{
                 return {
-                    errors:{
-                        ...errors,
-                        ...newErrors
-                    },
-                    values:{
-                        ...values,
-                        ...newValues
-                    }
+                    errors:newErrors,
+                    values:newValues
                 }
             });
+            
         }
         
 
