5ab3e63248f9c9178e74c79ca47e4e1da94e9c1a
Napat Maipaiboon <napat@DHA-iMac-8.local>
2017-10-24 20:31:50 +0700

    export ShowWhenDirty component

--- src/cf_form.js
+++ src/cf_form.js
@@ -1,11 +1,12 @@
 import withForm from './withForm/WithForm';
 import validator from './validator/validator';
 import errorMessage from './errorMessage/ErrorMessage';
-import {hasError} from './utils/utils';
+import {hasError,ShowWhenDirty} from './utils/utils';
 
 export {
     withForm,
     validator,
     errorMessage,
-    hasError
+    hasError,
+    ShowWhenDirtys
 };
